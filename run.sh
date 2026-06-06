907478a8627fc5f4fbc6c28eb6f6b793faa87945
hantang <34468720+hantang@users.noreply.github.com>
2026-06-06 17:20:47 +0800

    Update

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ readme_file="README.md"
 data_file="data.tsv"
 repo_file="repo_data.json"
 
-# readm update: macos: ggrep
+# readme update: macos: ggrep
 last_update=$(grep -oP '(?<=<!-- START-DATE -->\*)[0-9-]+(?=\*<!-- END-DATE -->)' $readme_file)
 today=$(date +%Y-%m-%d)
 
@@ -15,11 +15,11 @@ if [ "$last_update" == "$today" ]; then
     exit 0
 fi
 
-# echo "Inistall deps"
+# echo "Install deps"
 # pip install -r requirements.txt >/dev/null 2>&1
 
 echo "Update data"
-python src/run-stats.py -f $data_file -o $repo_file -b $batch
+python src/run-stats.py -f $data_file -o $repo_file -b "$batch"
 
 echo "Update readme"
 python src/run-doc.py -f $data_file -d $repo_file -o $readme_file
